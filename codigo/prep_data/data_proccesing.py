5471eb83cf44299291c317a7232ba28b07c825f6
Thibault Poux <thibault.poux@free.fr>
2024-05-04 23:27:45 +0200

    Getting some metrics about the model  we need to use, to select the better one

--- codigo/prep_data/data_proccesing.py
+++ codigo/prep_data/data_proccesing.py
@@ -3,31 +3,59 @@ import numpy as np
 import glob
 import cv2
 
-path_No = 'MLLD-2024-NPT/Dataset/no/*'
-path_Yes = 'MLLD-2024-NPT/Dataset/yes/*'
+path_No = 'Dataset/no/*'
+path_Yes = 'Dataset/yes/*'
 
-tumor = []
-no_tumor = []
-random_state = 42
+def load_data():
+    tumor = []
+    no_tumor = []
 
-for file in glob.iglob(path_Yes):
-    img = cv2.imread(file)      #Reading the images from the path
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)        #Changing the color from BGR to RGB
-    img = cv2.resize(img, (128, 128)) 
-    tumor.append((img, 1))  # Appending tuple with image and label 1 (indicating presence of tumor)
+    for file in glob.iglob(path_Yes):
+        img = cv2.imread(file)      #Reading the images from the path
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)        #Changing the color from BGR to RGB
+        img = cv2.resize(img, (128, 128)) 
+        tumor.append((img, 1))  # Appending tuple with image and label 1 (indicating presence of tumor)
 
-for file in glob.iglob(path_No):
-    img = cv2.imread(file)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (128, 128))
-    no_tumor.append((img, 0))  # Appending tuple with image and label 0 (indicating absence of tumor)
+    for file in glob.iglob(path_No):
+        img = cv2.imread(file)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (128, 128))
+        no_tumor.append((img, 0))  # Appending tuple with image and label 0 (indicating absence of tumor)
 
-# Concatenating the two lists and shuffle the data
-all_data = tumor + no_tumor
-
-
-# Splitting data and labels
-data = np.array([item[0] for item in all_data])
-labels = np.array([item[1] for item in all_data])
+    # Concatenating the two lists and shuffle the data
+    all_data = tumor + no_tumor
 
 
+    # Splitting data and labels
+    data = np.array([item[0] for item in all_data])
+    labels = np.array([item[1] for item in all_data])
+
+    return data, labels
+
+
+def load_data_flatten() :
+
+    data, labels = [], []
+    
+    # loop trough all the images in the dataset
+    for file in glob.iglob('Dataset/*/*.jpg'):
+        
+        # get the image, resize it, make it grey scale, and flatten it
+        img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (128, 128))
+        img = img.flatten()
+        data.append(img)
+        
+        # put the corect label
+        if 'no' in file:
+            labels.append(0)
+        else:
+            labels.append(1)
+    
+    return np.array(data), np.array(labels)
+
+# print some information about the data, when script executed
+if __name__ == '__main__':
+    
+    data, labels = load_data_flatten()
+    print(data.shape)
